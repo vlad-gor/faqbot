8594e1b751ec07c28044e9217fb1dfb08bfb6ed0
vlad-gor <gorunov341@yandex.ru>
2020-12-23 08:30:57 +0300

    danke

--- faqbot.py
+++ faqbot.py
@@ -82,12 +82,21 @@ async def change_language(message: types.Message):
 @dp.message_handler()
 async def get_question(message: types.Message):
     '''Находит ответ на вопрос и уточняет помогло ли решение'''
-    ans = classify_question(message)
-    await bot.send_message(message.chat.id, ans)
-    inline_btn_1 = InlineKeyboardButton('Yes', callback_data='Yes')
-    inline_btn_2 = InlineKeyboardButton('No', callback_data='No')
-    inline_kb1 = InlineKeyboardMarkup().add(inline_btn_1, inline_btn_2)
-    await bot.send_message(message.chat.id,"Have we resolved your problem?", reply_markup=inline_kb1)
+    current_user = dbM.get_user_from_id(message.chat.id)
+    if current_user.lang == 'English':
+        ans = classify_question(message)
+        await bot.send_message(message.chat.id, ans)
+        inline_btn_1 = InlineKeyboardButton('Yes', callback_data='Yes')
+        inline_btn_2 = InlineKeyboardButton('No', callback_data='No')
+        inline_kb1 = InlineKeyboardMarkup().add(inline_btn_1, inline_btn_2)
+        await bot.send_message(message.chat.id,"Have we resolved your problem?", reply_markup=inline_kb1)
+    elif current_user.lang == 'German':
+        ans = classify_question(message)
+        await bot.send_message(message.chat.id, ans)
+        inline_btn_1 = InlineKeyboardButton('Ja', callback_data='Ja')
+        inline_btn_2 = InlineKeyboardButton('Nein', callback_data='Nein')
+        inline_kb1 = InlineKeyboardMarkup().add(inline_btn_1, inline_btn_2)
+        await bot.send_message(message.chat.id,"Haben wir Ihr Problem gelöst?", reply_markup=inline_kb1)
 
 @dp.callback_query_handler(lambda c: c.data == 'Yes')
 async def process_callback_yes(callback_query: types.CallbackQuery):
@@ -95,9 +104,20 @@ async def process_callback_yes(callback_query: types.CallbackQuery):
     await bot.send_message(callback_query.from_user.id, 'Thank you for your request. We will be glad to see you soon.')
 
 @dp.callback_query_handler(lambda c: c.data == 'No')
-async def process_callback_yes(callback_query: types.CallbackQuery):
+async def process_callback_no(callback_query: types.CallbackQuery):
     await bot.answer_callback_query(callback_query.id)
     await bot.send_message(callback_query.from_user.id, 'Send you request to email support@citystore.world. We’ll solve your problem.')    
+
+@dp.callback_query_handler(lambda c: c.data == 'Ja')
+async def process_callback_yes(callback_query: types.CallbackQuery):
+    await bot.answer_callback_query(callback_query.id)
+    await bot.send_message(callback_query.from_user.id, 'Danke für ihre Anfrage. Wir freuen uns, Sie in naher Zukunft zu sehen.')
+
+@dp.callback_query_handler(lambda c: c.data == 'Nein')
+async def process_callback_no(callback_query: types.CallbackQuery):
+    await bot.answer_callback_query(callback_query.id)
+    await bot.send_message(callback_query.from_user.id, 'Senden Sie Ihre Anfrage an support@citystore.world. Wir werden Ihr Problem lösen.')
+
 
 def classify_question(message):
     '''Функция поиска ответа на вопрос'''
---
+++ reboot_service.sh
@@ -0,0 +1,4 @@
+systemctl stop faqbot
+systemctl status faqbot
+systemctl start faqbot
+systemctl status faqbot
