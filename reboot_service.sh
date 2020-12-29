25b071ecf19892cdb5ac24f473f01cccaf4a6b65
vlad-gor <gorunov341@yandex.ru>
2020-12-29 08:20:34 +0300

    pip3

--- reboot_service.sh
+++ reboot_service.sh
@@ -1,6 +1,7 @@
 systemctl stop faqbot
 systemctl status faqbot
-sudo pip3 install spacy
+sudo pip3 install -U pip setuptools wheel
+sudo pip3 install -U spacy
 sudo python3 -m spacy download de_core_news_sm
 sudo python3 -m spacy download en_core_web_sm
 systemctl start faqbot
